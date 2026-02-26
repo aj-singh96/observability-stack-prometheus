7794379ce175ac15a9fff15b7b4e358413908276
aj-singh96 <72389457+aj-singh96@users.noreply.github.com>
2026-02-26 13:02:51 +0000

    fix: properly format prod/variables.tf for terraform fmt compliance

--- terraform/environments/prod/variables.tf
+++ terraform/environments/prod/variables.tf
@@ -1,12 +1,64 @@
-variable "region" { default = "us-east-1" }
-variable "state_bucket" { description = "S3 bucket for Terraform state" type = string }
-variable "lock_table" { description = "DynamoDB table for state locking" type = string }
-variable "vpc_id" { type = string }
-variable "subnet_ids" { type = list(string) }
-variable "ami" { type = string }
-variable "key_name" { type = string }
-variable "instance_type" { default = "t3.small" }
-variable "instance_count" { default = 1 }
-variable "owner" { default = "team" }
-variable "environment" { default = "prod" }
-variable "allowed_cidr_blocks" { type = list(string) }
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "state_bucket" {
+  description = "S3 bucket for Terraform state"
+  type        = string
+}
+
+variable "lock_table" {
+  description = "DynamoDB table for state locking"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "VPC ID"
+  type        = string
+}
+
+variable "subnet_ids" {
+  description = "Subnet IDs"
+  type        = list(string)
+}
+
+variable "ami" {
+  description = "AMI ID"
+  type        = string
+}
+
+variable "key_name" {
+  description = "SSH key pair name"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.small"
+}
+
+variable "instance_count" {
+  description = "Number of instances"
+  type        = number
+  default     = 1
+}
+
+variable "owner" {
+  description = "Owner tag"
+  type        = string
+  default     = "team"
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "prod"
+}
+
+variable "allowed_cidr_blocks" {
+  description = "Allowed CIDR blocks for SSH"
+  type        = list(string)
+}
